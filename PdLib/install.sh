27acf8f7122d92e09e89055be6937edeb3929da2
damian <damian@frey.NOSPAMco.nz>
2010-07-12 01:12:50 +0800

    changed to use iphoneos3.2

--- PdLib/install.sh
+++ PdLib/install.sh
@@ -7,8 +7,8 @@ echo "Usage: $0 [prefix-dir]";
 exit 1;
 fi
 
-xcodebuild -sdk iphoneos3.1.2 -target pd "ARCHS=armv6 armv7" "VALID_ARCHS=armv6 armv7" build
-xcodebuild -sdk iphonesimulator3.1.2 -target pd "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" build
+xcodebuild -sdk iphoneos3.2 -target pd "ARCHS=armv6 armv7" "VALID_ARCHS=armv6 armv7" build
+xcodebuild -sdk iphonesimulator3.2 -target pd "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" build
 
 echo "Installing to $1" 
 
